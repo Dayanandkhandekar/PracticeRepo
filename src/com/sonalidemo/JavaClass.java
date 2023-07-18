400dd7406ce0ba3eea6d71cfee4a90b854be67ee
hp <hp@DESKTOP-QA5G25F>
2023-07-18 21:51:44 +0530

    sonali commiting 18-07-2023

--- src/com/sonalidemo/JavaClass.java
+++ src/com/sonalidemo/JavaClass.java
@@ -4,12 +4,25 @@ public class JavaClass {
 	//variable-name giving to memory location
 	//types of variables-instance,static,local
 	//syntax datatype variable ;
-	int s;//instance
+	int k;//instance
 	static int m;//static
-	public static void main(String[] args) {
-		int l;//local
-		System.out.println("hello welcome");
-	}
+
+	
+	
+
+	
+//datatype which type of value to be store
+	//there are two types primitive and non primitive data type
+	//primitive ----- we cannot create object of primitive
+	//numeric byte,short,int,long,float,double
+	//character   char c='a';(single character write here)
+	//boolean true/false
+	
+	//non primitive-----we can create object of primitive
+	//a.system defined data type eg.string,stringBuilder,stringbuffere,Allwrapperclasses,etc
+	//string s = "aaaaaa"(multiple character write)
+	//b.user defined data type ------all classes created by user
+	//eg-BasicDemo dd;
 	
 	//syntax 
    // datatype/returntype   methodname()  {//method body }
@@ -23,7 +36,6 @@ public class JavaClass {
 	}
     
 	{
-		
 		System.out.println("Inside instance block");
 		
 	}
@@ -31,5 +43,37 @@ public class JavaClass {
 	static {
 		System.out.println("Static block");
 	}
-	
+       
+
+
+
+
+	@Override
+	public String toString() {
+		return "JavaClass [k=" + k + "]";
+	}
+
+	//to create object
+			//1.using new keyword
+			//classname name=keyword classname();
+	public static void main(String[] args) {
+		System.out.println("welcome");
+		JavaClass b1=new JavaClass();
+		JavaClass b2=new JavaClass();
+		
+		
+		System.out.println("b1====="+b1);
+		System.out.println("b2====="+b2);
+		
+		
+		System.out.println("b1====="+b1.hashCode());
+		System.out.println("b2====="+b2.hashCode());
+		
+		System.out.println("b1====="+b1.getClass());
+		System.out.println("b2======"+b2.getClass());
+		
+	}
 }
+
+
+
