852e76b9b47f9fb8b3b604a2295e45e768690ee7
hp <hp@DESKTOP-QA5G25F>
2023-07-22 21:34:15 +0530

    sonali commited on constructor 22-07-23

--- src/com/sonalidemo/B.java
+++ src/com/sonalidemo/B.java
@@ -3,62 +3,112 @@ package com.sonalidemo;
 import java.util.Scanner;
 
 public class B {
-public static void main(String[] args, String sdf) {
-	employee e1=new employee();
-	employee e2=new employee();
+	public static void main(String[] args) {
+		student s1=new student();
+		student s2=new student();
+		student s3=new student();
+		
 	
-	e1.setEid1(1);
-	e1.setEname("sdf");
-	
-	 e2.setEid1(2);
-	 e2.setEname("pqr");
-	 
-	 
-	System.out.println("e1===="+e1);
-	System.out.println("e2===="+e2);
-	
-	 employee e3=new employee();
-	 Scanner sc=new Scanner(System.in);
-	 System.out.println("Enter ur Id");//5
-	 int id=sc.nextInt();
-	 e3.setSid(id);
-	 System.out.println("Enter ur name");// ss
-	 String name=sc.next();
-	 
-	
-}
+		s1.setSid(1);
+		s1.setSname("dfg");
+		s1.setMark(40);
+		
+		s2.setSid(2);
+		s2.setSname("tyu");
+		s2.setMark(70);
+		
+		s3.setSid(3);
+		s3.setSname("asd");
+		s3.setMark(90);
+		
+		
+		System.out.println("s1===="+s1);
+		System.out.println("s2===="+s2);
+		System.out.println("s3===="+s3);
+		
+		student s4= new student();
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter ur id");//5
+		int id=sc.nextInt();
+		s4.setSid(id);
+		System.out.println("enter ur name");//hh
+		String name=sc.next();
+		s4.setSname(name);
+		System.out.println("enter ur mark");
+		int mark=sc.nextInt();
+		s4.setMark(mark);
+		
+		
+		
+		
+		
+		System.out.println("s4===="+s4);
+		
+		student s5=new student(3,"yy",30);
+		student s6=new student(5,"ss",80);
+		
+	}
 
-@Override
-public String toString() {
-	return "B [getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
 }
-}
-class employee{
-	int eid1;
-	String ename;
-	int eid;
-	public int getEid1() {
-		return eid1;
-		
+class student{
+	int sid;
+	String sname;
+	int mark;
+	
+
+	@Override
+	public String toString() {
+		return "student [sid=" + sid + ", sname=" + sname + ", mark=" + mark + "]";
+			
 	}
-	public void setSid(int id) {
+
+
+	public String next() {
 		// TODO Auto-generated method stub
-		
+		return null;
 	}
-	public void setEid1(int eid1) {
-		this.eid1 = eid1;
+
+
+	public int nextInt() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
-	public String getEname() {
-		return ename;
+
+
+	public int getSid() {
+		return sid;
 	}
-		
-	public void setEname(String ename) {
-		this.ename = ename;
-}
-		
-}
-		
-		
-		
-		
-		
+
+
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+
+
+	public String getSname() {
+		return sname;
+	}
+
+
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+
+
+	public int getMark() {
+		return mark;
+	}
+
+
+	public void setMark(int mark) {
+		this.mark = mark;
+	}
+	
+	public student(int i, String string, int j) {
+	}
+
+
+	public student() {
+		// TODO Auto-generated constructor stub
+	}
+	}
