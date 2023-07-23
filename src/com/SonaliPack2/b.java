5263a4013e8a2ccebd1167d001ccace15bf00d29
hp <hp@DESKTOP-QA5G25F>
2023-07-23 20:44:04 +0530

    sonali commiting access modifier file 23-07-23

---
+++ src/com/SonaliPack2/b.java
@@ -0,0 +1,14 @@
+package com.SonaliPack2;
+
+import com.SonaliPack1.a;
+
+public class b extends a{
+  void m2() {
+	  a obj=new a();
+	  // System.out.println("a==="+obj.c);
+	  b obj1=new b();
+	  System.out.println("a==="+obj1.c);
+  }
+}
+	
+
