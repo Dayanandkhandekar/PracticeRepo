2ada70148b128f7cc0ad3cdf9d65f61aeea6d7d1
hp <hp@DESKTOP-QA5G25F>
2023-07-29 20:35:51 +0530

    sonali committing 29-07-23

--- src/com/sonalidemo/ConditionalStmtFirst.java
+++ src/com/sonalidemo/ConditionalStmtFirst.java
@@ -13,7 +13,9 @@ public static void main(String[] args) {
 	 
 	// IfDemoFirstSecond.m3();
 	 
-	 IfDemoFirstSecond.m4();
+	 //IfDemoFirstSecond.m4();
+	 
+	 IfDemoFirstSecond.m5();
 	 
 }
 }
@@ -65,6 +67,61 @@ class  IfDemoFirstSecond {
 			System.out.println("this no is not divisible by 3 and 4");
 		}
 		}
+	static void m5() {
+		Scanner sc = new Scanner (System.in);
+		 System.out.println("Enter first no");
+		 int num3 = sc.nextInt();
+		 System.out.println("Enter second no");
+		 int num4 = sc.nextInt();
+		 
+		 
+		 System.out.println("select opt");
+		 System.out.println("1. Add");
+		 System.out.println("2.sub");
+		 System.out.println("3.multi");
+		 System.out.println("4.div");
+		 System.out.println("5.check first num is even or odd");
+		 
+		 int opt=sc.nextInt();
+		 
+		 
+		if(opt==1) {
+			 int c=num3+num4;
+			 System.out.println( "Ans is "+ c);
+		 }
+			
+		else if(opt==2) {
+			 int c=num3-num4;
+			 System.out.println( "Ans is "+ c);
+		 }
+		else if(opt==3) {
+			 int c=num3*num4;
+			 System.out.println( "Ans is "+ c);
+		 }
+		else if(opt==4) {
+			 int c=num3/num4;
+			 System.out.println( "Ans is "+ c);
+		 }
+		else if(opt==5);
+		{
+		    int c=num3 % 2; 
+		    System.out.println("the no is even");
+		      System.out.println("the no is odd");
+			
+
+		}
+	}
+}
+
+		     
+		   
+		         
+	
+
+		        
+		     
+
+	
 		
 		
 			
@@ -79,4 +136,3 @@ class  IfDemoFirstSecond {
 	
 	
 	
-}
--- src/conditionalstmtdemofirst/ConditionalStmtFirst.java
+++ src/conditionalstmtdemofirst/ConditionalStmtFirst.java
@@ -13,7 +13,9 @@ public static void main(String[] args) {
 	 
 	// IfDemoFirstSecond.m3();
 	 
-	 IfDemoFirstSecond.m4();
+	 //IfDemoFirstSecond.m4();
+	 
+	 IfDemoFirstSecond.m5();
 	 
 }
 }
@@ -41,7 +43,6 @@ class  IfDemoFirstSecond {
 			
 	}
 	static void m3 () {
-		
 		Scanner sc = new Scanner (System.in);
 		 System.out.println("Enter first no");
 		 int num1 = sc.nextInt();
@@ -55,7 +56,6 @@ class  IfDemoFirstSecond {
 	static void m4() {
 		Scanner sc = new Scanner (System.in);
 		System.out.println("Enter the no");
-		
 		int num =sc.nextInt();
 		
 		if(num % 3 == 0 && num % 4 == 0)
@@ -67,5 +67,72 @@ class  IfDemoFirstSecond {
 			System.out.println("this no is not divisible by 3 and 4");
 		}
 		}
+	static void m5() {
+		Scanner sc = new Scanner (System.in);
+		 System.out.println("Enter first no");
+		 int num3 = sc.nextInt();
+		 System.out.println("Enter second no");
+		 int num4 = sc.nextInt();
+		 
+		 
+		 System.out.println("select opt");
+		 System.out.println("1. Add");
+		 System.out.println("2.sub");
+		 System.out.println("3.multi");
+		 System.out.println("4.div");
+		 System.out.println("5.check first num is even or odd");
+		 
+		 int opt=sc.nextInt();
+		 
+		 
+		if(opt==1) {
+			 int c=num3+num4;
+			 System.out.println( "Ans is "+ c);
+		 }
 			
+		else if(opt==2) {
+			 int c=num3-num4;
+			 System.out.println( "Ans is "+ c);
+		 }
+		else if(opt==3) {
+			 int c=num3*num4;
+			 System.out.println( "Ans is "+ c);
+		 }
+		else if(opt==4) {
+			 int c=num3/num4;
+			 System.out.println( "Ans is "+ c);
+		 }
+		else if(opt==5);
+		{
+		    int c=num3 % 2; 
+		    System.out.println("the no is even");
+		      System.out.println("the no is odd");
+			
+
+		}
+	}
 }
+
+		     
+		   
+		         
+	
+
+		        
+		     
+
+	
+		
+		
+			
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
