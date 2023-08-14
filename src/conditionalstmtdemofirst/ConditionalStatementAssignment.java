660418382165da83bad92fb3c2f6c17f8a2ba85e
hp <hp@DESKTOP-QA5G25F>
2023-08-14 12:53:59 +0530

    sonali committing correction 14-08-23

--- src/conditionalstmtdemofirst/ConditionalStatementAssignment.java
+++ src/conditionalstmtdemofirst/ConditionalStatementAssignment.java
@@ -1,50 +1,29 @@
 package conditionalstmtdemofirst;
 
-	public class ConditionalStatementAssignment {
-		public static void main(String[] args) {
-			//IfDemoFirst.m1();
-			//IfDemoFirst.m2();
-		    //IfDemoFirst.m3();
-			//IfDemoFirst.m4();
-			// IfDemoFirst.m5();
-			 
-		}
-
-
-		class IfDemoFirst {
-		 void m1() {
-			// 1-100 even no using for loop
-	          System.out.println("The first 100 even no");
-	           
-	          int num=100;
-	          for(int i=1;i<=100;i++) {
-	        	  if(i%2==0) {
-	        		  System.out.println(i);
-	        	  }
-	          }
-	}
-
-		public  void m61() {
-			// TODO Auto-generated method stub
-			 {
-	      			// TODO Auto-generated method stub
-	      			 {
-	      				// 100-1 odd no using do while loop
-	      				System.out.println("The 100 to first odd no");
-	      				
-	      				int i=100;
-	      				do {
-	      					if(i%2!=0) 
-	      			      		  System.out.println(i);
-	      			      		  i--;
-	      				}while(i>=1);
-	      			 }
-			 }
-			    }	
-		
-		
-
-		 void m2() {
+public class ConditionalStatementAssignment {
+public static void main(String[] args) {
+	//IfDemo.m1();
+	//IfDemo.m2();
+	//IfDemo.m3();
+	//IfDemo.m4();
+	//IfDemo.m5();
+	IfDemo.m5();
+	
+}
+}
+class IfDemo {
+	 static void m1() {
+		// 1-100 even no using for loop
+         System.out.println("The first 100 even no");
+          
+         int num=100;
+         for(int i=1;i<=100;i++) {
+       	  if(i%2==0) {
+       		  System.out.println(i);
+       	  }
+         }
+	 }
+	static void m2() {
 			// 1-100 even no using while loop
 	        System.out.println("The first 100 even no");
 	         
@@ -55,8 +34,8 @@ package conditionalstmtdemofirst;
 	      		  System.out.println(i);
 	      		  i++;
 	      	  }
-		}
-		 void m3() {
+	}
+	static void m3() {
 			// 1-100 even no using do while loop
 	        System.out.println("The first 100 even no");
 	         
@@ -68,44 +47,45 @@ package conditionalstmtdemofirst;
 	      		  i++;
 	      	  }while(i<=100);
 		} 
-
-		 void m4() {
+	 static void m4() {
 			// 100-1 odd no using for loop
+         System.out.println("The 100 to first odd no");
+          
+        
+         for(int i=100;i>=1;i--) {
+       	  if(i%2!=0) {
+       		  System.out.println(i);
+       	  }
+         }
+	 }
+	 static  void m5() {
+			// 100-1 odd no using while loop
 	          System.out.println("The 100 to first odd no");
 	           
-	         
-	          for(int i=100;i>=1;i--) {
-	        	  if(i%2!=0) {
-	        		  System.out.println(i);
-	        	  }
-	          }
-		}
-	           void m5() {
-	  			// 100-1 odd no using while loop
-	  	          System.out.println("The 100 to first odd no");
-	  	           
-	  	    
-	              int i=100;
-	              while(i>=1) {
-	      	  if(i%2!=0) 
-	      		  System.out.println(i);
-	      		  i--;  
-	              }
-	          }
-	             
-	        
-		}
-	}
-	
-	    		
-	
-	
-	
-		
-	
-	
-	 	        	 
-	  	        	 
-	          
-		
-	
+	    
+         int i=100;
+         while(i>=1) {
+ 	  if(i%2!=0) 
+ 		  System.out.println(i);
+ 		  i--;  
+         }
+     }
+	 static void m6() {
+	 
+			// 100-1 odd no using do while loop
+			System.out.println("The 100 to first odd no");
+			
+			int i=100;
+			do {
+				if(i%2!=0) 
+		      		  System.out.println(i);
+		      		  i--;
+			}while(i>=1);
+		 }
+
+	 
+	 
+}	 
+	 
+
+      
